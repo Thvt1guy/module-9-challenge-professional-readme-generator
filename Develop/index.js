5a6582dfc095bc9e2252a954d24594cb483abf03
[Derrick Alcius] <[derrickalcius@gmail.com]>
2023-03-09 22:42:14 -0500

    linked generate markdown to index.js and added questions to terminal

--- Develop/index.js
+++ Develop/index.js
@@ -1,13 +1,35 @@
 // TODO: Include packages needed for this application
+const fs = require('fs');
+const inquirer = require('inquirer');
+const path = require('path');
+const generateMarkdown = require('./utils/generateMarkdown');
+
 
 // TODO: Create an array of questions for user input
-const questions = [];
+const questions = [{
+    type: 'input',
+    name: 'title',
+    message: 'what will be your title name?'
+},
+{
+    type: 'input',
+    name: 'description',
+    message: 'what will be your description?'
+}
+];
+
 
 // TODO: Create a function to write README file
-function writeToFile(fileName, data) {}
+function writeToFile(fileName, data) {
+    return fs.writeFile(path.join(process.cwd(), fileName), data);
+}
 
 // TODO: Create a function to initialize app
-function init() {}
+function init() {
+    inquirer
+        .prompt(questions)
+        .then((answers) => { writeToFile('README.md', generateMarkdown(answers))})
+}
 
 // Function call to initialize app
 init();
--- Develop/utils/generateMarkdown.js
+++ Develop/utils/generateMarkdown.js
@@ -1,6 +1,11 @@
 // TODO: Create a function that returns a license badge based on which license is passed in
 // If there is no license, return an empty string
-function renderLicenseBadge(license) {}
+function renderLicenseBadge(license) {
+  if(license === "MIT"){
+    return "MIT BADE GCODE"
+  }
+
+}
 
 // TODO: Create a function that returns the license link
 // If there is no license, return an empty string
@@ -14,6 +19,8 @@ function renderLicenseSection(license) {}
 function generateMarkdown(data) {
   return `# ${data.title}
 
+
+  ${renderLicenseBadge(data.license)}
 `;
 }
 
